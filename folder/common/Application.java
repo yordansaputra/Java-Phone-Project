514eab04ecb203c3407e9610ff222e6060006290
yordansaputra <yordansaputra@hotmail.com>
2015-06-08 15:57:46 +0700

    Create Application.java

---
+++ folder/common/Application.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2014, Samuel I. Gunadi.
+ * All rights reserved.
+ * 
+ */
+
+package edu.phonesimulator.common;
+
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
+/**
+ * Application
+ * 
+ * @author Samuel I. Gunadi
+ */
+public abstract class Application
+extends JPanel
+{
+    protected String _id;
+    
+    public Application()
+    {
+        setDefaultPreferredSize();
+    }
+    
+    public String getAppId()
+    {
+        return _id;
+    }
+    
+    public void setDefaultPreferredSize()
+    {
+        setPreferredSize(new Dimension(ApplicationManager.getMgr().getContainer().getPreferredSize()));
+    }
+    
+    public boolean sendBackKeyInterrupt()
+    {
+        return false;
+    }
+};
